a902725fbdf23d903b292cbae4961ab4a0e375ef
AryanBarot0011 <aryanbarot0011@gmail.com>
2026-07-12 18:19:57 +0530

    Change address

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,6 +4,6 @@ const CONFIG = {
   phone: "+91 99255 92042",
   whatsapp: "+91 9925592042",
   email: "swayambhuayurvedam@gmail.com",
-  address: "Tirupati Clinic, Near Parvati Jadav Hospital, Naroda, Ahmedabad, Gujarat - 382330",
+  address: "Swayambhu Ayurvedam Near Parvati Jadav Hospital,Opp.The Union Co- operative Bank Ltd.,Naroda, Ahmedabad – 382330,Gujarat, India.",
   mapEmbed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.1895694726593!2d72.65156287514339!3d23.075595099999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e81292e0e51eb%3A0xee7a8d08e96d21c9!2sTirupati%20clinic!5e0!3m2!1sen!2sin!4v1719326400000!5m2!1sen!2sin"
 };
